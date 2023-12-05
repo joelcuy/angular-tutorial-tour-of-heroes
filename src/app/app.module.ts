f0458c9d2f9bc85873263b92e53dad772fb8e37a
Joel Cheah <gjoelcheah@gmail.com>
2023-12-05 19:42:03 +0000

    Put messages into a separate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,18 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { HeroesModule } from './heroes/heroes.module';
+import { MessagesModule } from './messages/messages.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MessagesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HeroesModule
+    HeroesModule,
+    MessagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
